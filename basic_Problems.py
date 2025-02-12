0d11a9ed5f5e8d88e632a609c8fee7b6fa8eaa4c
Mathavan <[valid-email]>
2025-02-12 06:47:14 +0530

    hacker rank

---
+++ basic_Problems.py
@@ -0,0 +1,52 @@
+# -- Given an integer, , perform the following conditional actions:
+
+# -- If n is odd, print Weird
+# -- If n is even and in the inclusive range of 2 to 5, print Not Weird
+# -- If n is even and in the inclusive range of 6 to 20 , print Weird
+# -- If n is even and greater than 20 , print Not Weird -->
+# -- <!-- 
+# -- Output Format
+
+# -- Print Weird if the number is weird. Otherwise, print Not Weird.
+
+# n=int(input("enter:"))
+# if(n%2==0) or (n>=6 and n<=20):
+#     print("Not weird")
+# if(n>=2 and n<=5):
+#      print(" Weird")
+
+# Task
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+# a=int(input("enter a num_1="))
+# b=int(input("enter a num_2="))
+# c=a+b
+# d=a-b
+# e=a*b
+# print(c)
+# print(d)
+# print(e)
+
+# Task
+# The provided code stub reads two integers,  and , from STDIN.
+
+# Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+
+# No rounding or formatting is necessary
+
+# a = int(input())
+# b = int(input())
+# c=a//b
+# d=a/b
+# print(c)
+# print(d)
+
+# The provided code stub reads and integer,n , from STDIN. For all non-negative integers i<n , print i*i.
+
+# n=int(input())
+# for i in range(0,n):
+#     print(i*i)
